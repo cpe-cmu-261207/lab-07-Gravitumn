d3a9d95cee4718f689959d6902c474da7499ce81
leekyungwoo <thanisorn_c@cmu.ac.th>
2021-09-03 19:25:31 +0700

    golden time

--- src/components/SelectColorButton.tsx
+++ src/components/SelectColorButton.tsx
@@ -1,18 +1,22 @@
+import { colorChange, PixelPainterStore } from "../stores/PixelPainterStore"
+
 type SelectColorButtonProps = {
   color: string;
 }
 
 const SelectColorButton = ({ color }: SelectColorButtonProps) => {
-
+  const current = PixelPainterStore.useState(s=>s.current)
   //modify this function to highlight correctly
   const computeRingSize = () => {
-    return ""
-    return "ring-8 ring-green-400"
+    if(color === current)
+      return "ring-8 ring-green-400"
+    else
+      return " "
   }
 
   return (
     <div className={`${computeRingSize()} rounded-md border-black border-2 w-12 h-12 cursor-pointer`}
-      style={{ backgroundColor: color }}
+      style={{ backgroundColor: color }} onClick={()=>colorChange(color)}
     >
     </div>
   )
--- src/stores/PixelPainterStore.ts
+++ src/stores/PixelPainterStore.ts
@@ -3,6 +3,7 @@ import {Store} from 'pullstate'
 type PixelPainterStoreType = {
   //we save painted color as hex code (string) in 2D array
   canvas: string[][] 
+  current: string
 }
 
 //return an (16 x 16) 2D array filled with "#FFFFFF"
@@ -18,5 +19,68 @@ const createEmptyCanvas = () => {
 }
 
 export const PixelPainterStore = new Store<PixelPainterStoreType>({
-  canvas: createEmptyCanvas()
+  canvas: createEmptyCanvas(),
+  current: '#000000'
 })
+
+export const colorChange = (color:string) => {
+  PixelPainterStore.update(
+    s=> {s.current = color}
+  )
+}
+
+export const CanvasUpdate = (x:number,y:number) => {
+  PixelPainterStore.update(
+    s =>{
+      s.canvas[y][x] = s.current
+    }
+  )
+}
+
+export const clearCanvas = () =>{
+  PixelPainterStore.update(
+    s => {
+      s.canvas = createEmptyCanvas()
+    }
+  )
+}
+
+export const RandomCanvas = () =>{
+  const output: string[][] = []
+  for (let i=0; i<16; i++){
+    output[i] = []
+    for (let j=0; j<16; j++){
+      let randnum = Math.floor(Math.random() * 12)+1;
+      if(randnum === 1)
+        output[i].push('#000000')
+      else if(randnum === 2)
+        output[i].push('#804000')
+      else if(randnum === 3)
+        output[i].push('#FE0000')
+      else if(randnum === 4)
+        output[i].push('#FE6A00')
+      else if(randnum === 5)
+        output[i].push('#FFD800')
+      else if(randnum === 6)
+        output[i].push('#FFFFFF')
+      else if(randnum === 7)
+        output[i].push('#01FFFF')
+      else if(randnum === 8)
+        output[i].push('#0094FE')
+      else if(randnum === 9)
+        output[i].push('#0026FF')
+      else if(randnum === 10)
+        output[i].push('#B100FE')
+      else if(randnum === 11)
+        output[i].push('#FF006E')
+      else if(randnum === 12)
+        output[i].push('#00FF01')
+    }
+  }
+
+  PixelPainterStore.update(
+    s=>{
+      s.canvas = output
+    }
+  )
+}
